c6c2927231d86a7e07f30b3a3e6fc4e5321d5022
MinMint <minmintworkspace@gmail.com>
2025-07-21 17:43:20 +0700

    fix CORS syntax

--- Controllers/Program.cs
+++ Controllers/Program.cs
@@ -85,7 +85,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAllOrigins",
         builder =>
         {
-            builder.AllowAnyOrigin( "http://localhost:5173", "http://localhost:3000")
+            builder.WithOrigins("http://localhost:5173", "http://localhost:3000")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
